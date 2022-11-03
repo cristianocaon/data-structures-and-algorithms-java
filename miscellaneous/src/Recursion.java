e23ff4bb9554847a5932bfae62a0ddbab95a377c
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-11-03 12:59:37 -0500

    recursion factorial

--- miscellaneous/src/Recursion.java
+++ miscellaneous/src/Recursion.java
@@ -1,6 +1,7 @@
 public class Recursion {
     public static void main(String[] args) {
         System.out.println(iterativeFactorial(5));
+        System.out.println(recursiveFactorial(5));
     }
 
     public static int iterativeFactorial(int num) {
@@ -9,10 +10,17 @@ public class Recursion {
         }
 
         int factorial = 1;
-        for (int i = 1; i < num; i++) {
+        for (int i = 1; i <= num; i++) {
             factorial *= i;
         }
 
         return factorial;
+    }
+
+    public static int recursiveFactorial(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        return num * recursiveFactorial(num - 1);
     }
 }
